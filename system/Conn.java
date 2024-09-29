4bc448d5481e89bd22cdb9c250c80b138531f450
kavyasp20 <149318455+kavyasp20@users.noreply.github.com>
2024-09-29 21:48:26 +0530

    Add files via upload

---
+++ system/Conn.java
@@ -0,0 +1,24 @@
+
+package travel.management.system;
+
+import java.sql.*;
+
+public class Conn {
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql://localhost:3306/travelmanagementsystem","root","kavyasree"); 
+            
+            s =c.createStatement();  
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
+    
+    
+
